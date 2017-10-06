576a4aa01055909c6f9a03dd2d4f0a33894a6c77
Wondae <bsmithx479x@gmail.com>
2017-10-05 20:10:10 -0500

    exercise19

--- Exercise19/src/scripts/main.js
+++ Exercise19/src/scripts/main.js
@@ -10,26 +10,31 @@ $(document).ready(function () {
     function savePost(e) {
         e.preventDefault();
         var postObj = $('#frm').serialize();
-        $.post(url, postObj, function(post) {
+        $.post(url, postObj, function (post) {
             console.log(post);
             loadPosts();
-            
+
         });
     }
 
     function loadPosts() {
-        $.get(url, function(resp) {
-            ///convert HTML
-            var html = convertResponseToHTML(resp);
-            ///apply the html
-            $("#main").html(html);
+
+        $.get(url, function (response) {
+
+            //Convert HTML Stuff
+            var convertedHTML = convertResponseToHTML(response);
+
+            //Apply the html stuff
+            $('#main').html(convertedHTML);
+            console.log(response);
+
         });
     }
 
 
     function convertResponseToHTML(resp) {
         var html = '';
-        
+
         for (var i = 0; i < resp.length; i++) {
             var post = resp[i];
             var postHTML = '<div class="post">';
